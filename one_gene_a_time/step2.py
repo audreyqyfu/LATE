27c42be7e03b891b89e4cac8bb58e7a886cbd73c
“Rui <liruiradiant@gmail.com>
2017-08-01 00:33:28 -0700

    One-gene-a-time runs, but only fnn:bias updated, fnn:weights did not learn, prediction pearsonr bad

--- one_gene_a_time/step2.py
+++ one_gene_a_time/step2.py
@@ -1,9 +1,15 @@
 # load pre-trained NN, transfer learning
 # https://www.tensorflow.org/get_started/summaries_and_tensorboard
 # 07/25/2017
-import tensorflow as tf
+# 07/31/2017 One Gene a time
+from __future__ import division #fix division // get float bug
+from __future__ import print_function #fix printing \n
+
+import tensorflow as tf; print('tf.__version__', tf.__version__)
+import sys; print ('python version:', sys.version)
 import numpy as np
 import pandas as pd
+import matplotlib; matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr
 import math
@@ -16,9 +22,9 @@ def split_arr(arr, a=0.8, b=0.1, c=0.1):
     a: train, b: valid, c: test
     e.g.: [arr_train, arr_valid, arr_test] = split(df.values)"""
     np.random.seed(1) # for splitting consistency
-    train_indices = np.random.choice(arr.shape[0], round(arr.shape[0] * a/(a+b+c)), replace=False)
+    train_indices = np.random.choice(arr.shape[0], int(arr.shape[0] * a/(a+b+c)), replace=False)
     remain_indices = np.array(list(set(range(arr.shape[0])) - set(train_indices)))
-    valid_indices = np.random.choice(remain_indices, round(len(remain_indices) * b/(b+c)), replace=False)
+    valid_indices = np.random.choice(remain_indices, int(len(remain_indices) * b/(b+c)), replace=False)
     test_indices = np.array(list( set(remain_indices) - set(valid_indices) ))
     np.random.seed() # cancel seed effect
     print("total samples being split: ", len(train_indices) + len(valid_indices) + len(test_indices))
@@ -35,9 +41,9 @@ def split_df(df, a=0.8, b=0.1, c=0.1):
     a: train, b: valid, c: test
     e.g.: [df_train, df2, df_test] = split(df, a=0.7, b=0.15, c=0.15)"""
     np.random.seed(1) # for splitting consistency
-    train_indices = np.random.choice(df.shape[0], round(df.shape[0] * a/(a+b+c)), replace=False)
+    train_indices = np.random.choice(df.shape[0], int(df.shape[0] * a/(a+b+c)), replace=False)
     remain_indices = np.array(list(set(range(df.shape[0])) - set(train_indices)))
-    valid_indices = np.random.choice(remain_indices, round(len(remain_indices) * b/(b+c)), replace=False)
+    valid_indices = np.random.choice(remain_indices, int(len(remain_indices) * b/(b+c)), replace=False)
     test_indices = np.array(list( set(remain_indices) - set(valid_indices) ))
     np.random.seed() # cancel seed effect
     print("total samples being split: ", len(train_indices) + len(valid_indices) + len(test_indices))
@@ -57,7 +63,14 @@ def medium_corr(arr1, arr2, num=100, accuracy = 3):
     for i in range(num - 1):
         pearsonrlog.append(pearsonr(arr1[i], arr2[i]))
     pearsonrlog.sort()
-    result = round(pearsonrlog[round(num/2)][0], accuracy)
+    result = round(pearsonrlog[int(num/2)][0], accuracy)
+    return(result)
+
+def medium_corr_one_gene(col1, col2, accuracy = 3):
+    """will calculate pearsonr for gene(i)"""
+    # from scipy.stats.stats import pearsonr
+    result = pearsonr(col1, col2)[0]
+    # result = round(result, accuracy)
     return(result)
 
 def save_hd5 (df, out_name):
@@ -87,8 +100,8 @@ def scatterplot(x, y, title, xlabel, ylabel):
     plt.savefig(title + '.png', bbox_inches='tight')
 
 # read data #
-file = "../data/splat_v1-1-2/splat.OneGroup.norm.log.B.mask90.hd5" #data need imputation
-file_benchmark = "../data/splat_v1-1-2/splat.OneGroup.norm.log.B.hd5" #data need imputation
+file = "../../data/splat_v1-1-2/splat.OneGroup.norm.log.B.hd5" #data need imputation
+file_benchmark = "../../data/splat_v1-1-2/splat.OneGroup.norm.log.B.hd5" #data need imputation
 df = pd.read_hdf(file).transpose() #[cells,genes]
 df2 = pd.read_hdf(file_benchmark).transpose() #[cells,genes]
 m, n = df.shape  # m: n_cells; n: n_genes
@@ -105,12 +118,12 @@ df2_test = df2.ix[df_test.index]
 
 # Parameters #
 learning_rate = 0.001
-training_epochs = 150
-batch_size = 128
+training_epochs = 2000
+batch_size = 256
 sd = 0.01 #stddev for random init
 
-display_step = 1
-snapshot_step = 10
+display_step = 10
+snapshot_step = 1000
 
 # Network Parameters #
 n_input = n
@@ -146,8 +159,12 @@ corr_log = []
 epoch_log = []
 
 # regression #
+gene_index=2 #can loop in range(n)
+print('gene index: ', gene_index)
+
 X = tf.placeholder(tf.float32, [None, n_input])  # input
-Y = tf.placeholder(tf.float32, [None, n_input])  # benchmark
+M = tf.placeholder(tf.float32, [None, n_input])  # benchmark
+# Y = tf.placeholder(tf.float32, [None, 1]) # for a gene
 
 # weights = {
 #     'encoder_w1': tf.Variable(tf.random_normal([n_input, n_hidden_1], stddev= sd), name='encoder_w1'),
@@ -172,8 +189,8 @@ biases = {
     'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2], stddev= sd)),
     'decoder_b1': tf.Variable(tf.random_normal([n_hidden_1], stddev= sd)),
     'decoder_b2': tf.Variable(tf.random_normal([n_input], stddev= sd)),
-}
 
+}
 
 def encoder(x):
     with tf.name_scope("Encoder"):
@@ -209,23 +226,43 @@ def decoder(x):
         variable_summaries('activations_w2', layer_2)
     return layer_2
 
+def focusFnn(x):
+    """output shape is [m, 1]"""
+    with tf.name_scope("focusFnn"):
+        layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['decoder_w1']),
+                                       biases['decoder_b1']))
+        layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, tf.reshape(weights['decoder_w2'][:,gene_index], [weights['decoder_w2'].get_shape().as_list()[0], 1]) ),
+                                       biases['decoder_b2'][gene_index]))
+
+        variable_summaries('weights_w1', weights['decoder_w1'])
+        variable_summaries('weights_w2', weights['decoder_w2'])
+        variable_summaries('biases_b1', biases['decoder_b1'])
+        variable_summaries('biases_b2', biases['decoder_b2'])
+        variable_summaries('activations_w1', layer_1)
+        variable_summaries('activations_w2', layer_2)
+    return layer_2
+
 # Construct model
 encoder_op = encoder(X)
-decoder_op = decoder(encoder_op)
+focusFnn_op = focusFnn(encoder_op)  # for one gene a time prediction
+decoder_op = decoder(encoder_op)  # for pearson correlation of the whole matrix
 
 # Prediction
-y_pred = decoder_op
+y_pred = focusFnn_op # [m, 1]
 # Targets (Labels) are the input data.
-y_true = X
-y_benchmark = Y
+y_true = X[:, gene_index]
+y_benchmark = M[:, gene_index]
 
 # Define loss and optimizer, minimize the squared error
 with tf.name_scope("Metrics"):
-    cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
-    cost_benchmark = tf.reduce_mean(tf.pow(y_pred- y_benchmark, 2))
-    tf.summary.scalar('cost', cost)
-    tf.summary.scalar('cost_benchmark', cost_benchmark)
-
+    cost_fnn = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+    cost_fnn_benchmark = tf.reduce_mean(tf.pow(y_pred- y_benchmark, 2))
+    cost_decoder = tf.reduce_mean(tf.pow(X - decoder_op, 2))
+    cost_decoder_benchmark = tf.reduce_mean(tf.pow(decoder_op - M, 2))
+    tf.summary.scalar('cost_fnn', cost_fnn)
+    tf.summary.scalar('cost_fnn_benchmark', cost_fnn_benchmark)
+    tf.summary.scalar('cost_decoder', cost_decoder)
+    tf.summary.scalar('cost_decoder_benchmark', cost_decoder_benchmark)
 
 fine_tune_varList = [
                   weights['encoder_w1'],
@@ -249,8 +286,8 @@ last_layer_train_varList = [
                     ]
 
 optimizer = tf.train.RMSPropOptimizer(learning_rate).\
-    minimize(cost, var_list=decoder_train_varList)  # frozen other variables
-print("# Updated layers: ", "decoder\n")
+    minimize(cost_fnn, var_list=decoder_train_varList)  # frozen other variables
+print("# Updated layers: ", "focusFnn\n")
 
 
 # Initializing the variables
@@ -271,18 +308,18 @@ train_writer = tf.summary.FileWriter(log_dir+'/train', sess.graph)
 valid_writer = tf.summary.FileWriter(log_dir+'/valid', sess.graph)
 # benchmark_writer = tf.summary.FileWriter(log_dir+'/benchmark', sess.graph)
 
-# # Evaluate the init network
-cost_train = sess.run(cost, feed_dict={X: df_train.values})
-cost_valid = sess.run(cost, feed_dict={X: df_valid.values})
+# Evaluate the init network
+cost_train = sess.run(cost_fnn, feed_dict={X: df_train.values, M: df2_train.values})
+cost_valid = sess.run(cost_fnn, feed_dict={X: df_valid.values, M: df2_valid.values})
 print("\nTransferred network at Epoch 0: cost_train=", round(cost_train,3), "cost_valid=", round(cost_valid,3))
 
-h_train = sess.run(y_pred, feed_dict={X: df_train.values[:100]})
-h_valid = sess.run(y_pred, feed_dict={X: df_valid.values[:100]})
-print("medium benchmark_pearsonr in first 100 train cells: ", medium_corr(df2_train.values, h_train))
-print("medium benchmark_pearsonr in first 100 valid cells: ", medium_corr(df2_valid.values, h_valid))
+h_train = sess.run(y_pred, feed_dict={X: df_train.values})
+h_valid = sess.run(y_pred, feed_dict={X: df_valid.values})
+print("medium benchmark_pearsonr for gene ", gene_index, " in training cells :", medium_corr_one_gene(df2_train.values[:,gene_index:(gene_index+1)], h_train))
+print("medium benchmark_pearsonr for gene ", gene_index, " in valid cells:", medium_corr_one_gene(df2_valid.values[:,gene_index:(gene_index+1)], h_valid))
 
 # Train
-total_batch = math.floor(len(df_train)/batch_size)  # floor
+total_batch = int(math.floor(len(df_train)/batch_size))  # floor
 # Training cycle,step2
 for epoch in range(1, training_epochs+1):
     tic_cpu = time.clock(); tic_wall = time.time()
@@ -291,15 +328,16 @@ for epoch in range(1, training_epochs+1):
     for i in range(total_batch):
         indices = np.arange(batch_size*i, batch_size*(i+1))
         batch_xs = df_train.values[indices,:]
-        _, cost_batch = sess.run([optimizer, cost], feed_dict={X: batch_xs})
+        _, cost_batch = sess.run([optimizer, cost_fnn], feed_dict={X: batch_xs})
     if (batch_size*(i+1) < len(df_train)):
         indices = np.arange(batch_size*(i+1), len(df_train))
         batch_xs = df_train.values[indices,:]
-        _, cost_batch = sess.run([optimizer, cost], feed_dict={X: batch_xs})
+        _, cost_batch = sess.run([optimizer, cost_fnn], feed_dict={X: batch_xs})
     toc_cpu = time.clock(); toc_wall = time.time()
 
     # Log per epoch
     if (epoch == 1) or (epoch % display_step == 0):
+        tic_log = time.time()
         print("\n#Epoch ", epoch, " took: ",
               round(toc_cpu - tic_cpu, 1), " CPU seconds; ",
               round(toc_wall - tic_wall, 1), "Wall seconds")
@@ -307,44 +345,47 @@ for epoch in range(1, training_epochs+1):
         run_metadata = tf.RunMetadata()
         train_writer.add_run_metadata(run_metadata, 'epoch%03d' % epoch)
 
-        h_train = sess.run(y_pred, feed_dict={X: df_train.values[:100]})
-        h_valid = sess.run(y_pred, feed_dict={X: df_valid.values[:100]})
-        corr_train = medium_corr(df2_train.values, h_train)
-        corr_valid = medium_corr(df2_valid.values, h_valid)
-        print("medium pearsonr in first 100 train cells: ", corr_train)
-        print("medium pearsonr in first 100 valid cells: ", corr_valid)
+        h_train = sess.run(y_pred, feed_dict={X: df_train.values})
+        h_valid = sess.run(y_pred, feed_dict={X: df_valid.values})
+
+        corr_train = medium_corr_one_gene(df2_train.values[:, gene_index:(gene_index + 1)], h_train)
+        corr_valid = medium_corr_one_gene(df2_valid.values[:, gene_index:(gene_index + 1)], h_valid)
+        print("medium benchmark_pearsonr for gene ", gene_index, " in training cells :", corr_train)
+        print("medium benchmark_pearsonr for gene ", gene_index, " in valid cells:", corr_valid)
         corr_log.append(corr_valid)
         epoch_log.append(epoch)
 
         #Summary
         merged = tf.summary.merge_all()
 
-        [summary_train, cost_train] = sess.run([merged, cost], feed_dict={X: df_train.values, Y: df2_train.values})
-        [summary_valid, cost_valid] = sess.run([merged, cost], feed_dict={X: df_valid.values, Y: df2_valid.values})
+        [summary_train, cost_train] = sess.run([merged, cost_fnn], feed_dict={X: df_train.values, M: df2_train.values})
+        [summary_valid, cost_valid] = sess.run([merged, cost_fnn], feed_dict={X: df_valid.values, M: df2_valid.values})
         train_writer.add_summary(summary_train, epoch)
         valid_writer.add_summary(summary_valid, epoch)
 
         print("cost_batch=", "{:.6f}".format(cost_batch),
               "cost_train=", "{:.6f}".format(cost_train),
               "cost_valid=", "{:.6f}".format(cost_valid))
+        toc_log=time.time()
+
+        # #test
+        print ("test w_decoder_w2:", sess.run(weights['decoder_w2'][0:5, 0:5]))
+        print('log time for each display:', round(toc_log-tic_log, 1))
 
     # Log per observation interval
     if (epoch == 1) or (epoch % snapshot_step == 0) or (epoch == training_epochs):
+        tic_log2 = time.time()
         print("#Snapshot: ")
-        # show full data-set corr
-        h_train = sess.run(y_pred, feed_dict={X: df_train.values})  # np.array [len(df_train),1]
-        h_valid = sess.run(y_pred, feed_dict={X: df_valid.values})
-        print("medium pearsonr in all training cells: ", medium_corr(df2_train.values, h_train, num=len(df_train)))
-        print("medium pearsonr in all validing cells: ", medium_corr(df2_valid.values, h_valid, num=len(df_valid)))
         # save predictions
         h_input = sess.run(y_pred, feed_dict={X: df.values})
-        print("medium pearsonr in all imputation cells: ", medium_corr(df2.values, h_input, num=m))
-        df_h_input = pd.DataFrame(data=h_input, columns=df.columns, index=df.index)
-        save_hd5(df_h_input, log_dir+"/imputation.hd5")
+        df_h_input = pd.DataFrame(data=h_input, columns=[gene_index], index=df.index)
+        save_hd5(df_h_input, log_dir+"/h."+str(gene_index)+".hd5")
 
         # save model
         save_path = saver.save(sess, log_dir+"/step2.ckpt")
         print("Model saved in: %s" % save_path)
+        toc_log2 = time.time()
+        print ('log2 time for observation intervals:', round(toc_log2 - tic_log2, 1))
 
 
 train_writer.close()
