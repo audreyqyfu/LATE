8dbd771824cea7c01958baae400b273949abe9b2
“Rui <liruiradiant@gmail.com>
2017-10-17 02:05:26 -0400

    less tracked by variable_summaries()

--- one_gene_a_time/2steps3layers/bin/scimpute.py
+++ one_gene_a_time/2steps3layers/bin/scimpute.py
@@ -517,13 +517,13 @@ def read_data(data_name):
 def variable_summaries(name, var):
     """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
     with tf.name_scope(name):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
+        # mean = tf.reduce_mean(var)
+        # tf.summary.scalar('mean', mean)
+        # with tf.name_scope('stddev'):
+        #     stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        # tf.summary.scalar('stddev', stddev)
+        # tf.summary.scalar('max', tf.reduce_max(var))
+        # tf.summary.scalar('min', tf.reduce_min(var))
         tf.summary.histogram('histogram', var)
 
 
