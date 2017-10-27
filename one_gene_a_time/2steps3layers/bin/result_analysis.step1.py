e54ef2ea499a2e3eb1965db00e3a3b3098e96ab6
“Rui <liruiradiant@gmail.com>
2017-10-26 17:40:56 -0700

    add truth-pred scatterplot

--- one_gene_a_time/2steps3layers/bin/result_analysis.step1.py
+++ one_gene_a_time/2steps3layers/bin/result_analysis.step1.py
@@ -13,36 +13,60 @@ import os
 import time
 import scimpute
 
-# read data
+# set filename
 file_input = "../../../../magic/results/mouse_bone_marrow/EMT_MAGIC_9k/EMT.MAGIC.9k.A.log.hd5"  # data need imputation
+file_groundTruth = "../../../../magic/results/mouse_bone_marrow/EMT_MAGIC_9k/EMT.MAGIC.9k.A.log.hd5"  # data need imputation
 file_pred = 'pre_train/imputation.step1.hd5'
 
-df_pred = scimpute.read_hd5('pre_train/imputation.step1.hd5')
-df_input = scimpute.read_hd5(file_input)
-df_input = scimpute.subset_df(df_input, df_pred)
+# read data
+h = scimpute.read_hd5('pre_train/imputation.step1.hd5')
+df = scimpute.read_hd5(file_input).transpose()
+# df = scimpute.subset_df(df, h)
+print('df.shape', df.shape)
+df2 = scimpute.read_hd5(file_groundTruth).transpose()
+# df2 = scimpute.subset_df(df2, h)
+print('df2.shape', df2.shape)
+print('h:', h.ix[0:4, 0:4])
+print('input:', df.ix[0:4, 0:4])
+print('groundTruth:', df2.ix[0:4, 0:4])
 
+# read index
 train_idx = scimpute.read_csv('pre_train/df_train.index.csv').index
 valid_idx = scimpute.read_csv('pre_train/df_valid.index.csv').index
+# split
+h_train = h.ix[train_idx]
+h_valid = h.ix[valid_idx]
+df_train = df.ix[train_idx]
+df_valid = df.ix[valid_idx]
+df2_train = df2.ix[train_idx]
+df2_valid = df2.ix[valid_idx]
 
-df_input_train = df_input.ix[train_idx]
-df_input_valid = df_input.ix[valid_idx]
+# truth vs pred
+def groundTruth_vs_prediction():
+    print("> Ground truth vs prediction")
+    for j in [1, 2, 3, 4, 205, 206, 4058, 7496, 8495, 12871]:  # Cd34, Gypa, Klf1, Sfpi1
+            scimpute.scatterplot2(df2_valid.values[:, j], h_valid.values[:, j], range='same',
+                                  title=str('scatterplot1, gene-' + str(j) + ', valid, step1'),
+                                  xlabel='Ground Truth ',
+                                  ylabel='Prediction '
+                                  )
+            scimpute.scatterplot2(df2_valid.values[:, j], h_valid.values[:, j], range='flexible',
+                                      title=str('scatterplot2, gene-' + str(j) + ', valid, step1'),
+                                      xlabel='Ground Truth ',
+                                      ylabel='Prediction '
+                                      )
 
-df_groundTruth = scimpute.read_hd5(file_groundTruth)
-df_groundTruth = scimpute.subset_df(df_groundTruth, df_pred)
-
-print(df_pred.ix[0:4, 0:4])
-print(df_input.ix[0:4, 0:4])
-print(df_groundTruth.ix[0:4, 0:4])
+groundTruth_vs_prediction()
 
 # gene MSE
 j = 0
-input_j = df_input.ix[:, j:j+1].values
-pred_j = df_pred.ix[:, j:j+1].values
-groundTruth_j = df_groundTruth.ix[:, j:j+1].values
+input_j = df.ix[:, j:j+1].values
+pred_j = h.ix[:, j:j+1].values
+groundTruth_j = df2.ix[:, j:j+1].values
 
 mse_j_input = ((pred_j - input_j) ** 2).mean()
 mse_j_groundTruth = ((pred_j - groundTruth_j) ** 2).mean()
 
 # matrix MSE
-matrix_mse_input = ((df_pred.values - df_input.values) ** 2).mean()
-matrix_mse_groundTruth = ((df_pred.values - df_groundTruth.values) ** 2).mean()
+matrix_mse_input = ((h.values - df.values) ** 2).mean()
+matrix_mse_groundTruth = ((h.values - df2.values) ** 2).mean()
