592515120df7970ef3a49da775f22c58e8c641d8
“Rui <liruiradiant@gmail.com>
2017-11-07 17:21:29 -0800

    Use SVD to do PCA variation plot

---
+++ one_gene_a_time/2steps3layers/bin/svd_variation_plot.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# read gene expression matrix into df [genes, cells]
+# perform randomized SVD to know the internal dimension complexity of datasets
+
+import numpy as np
+import pandas as pd
+import pylab
+from sklearn.utils.extmath import randomized_svd #https://stackoverflow.com/questions/31523575/get-u-sigma-v-matrix-from-truncated-svd-in-scikit-learn
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt
+import time
+import os
+import sys
+
+# Usage
+print('usage: <svd_variation_plot.py> <input.hd5> <out_prefix> <n_rank>')
+print('default input is a gene expression matrix [cell, gene]')
+print('so that df in program is [gene, cell]')
+print('n_rank is the num of dims you want to check, if not specified, will be n_gene')
+print(sys.argv)
+
+file = sys.argv[1]
+out_prefix = sys.argv[2]
+
+# Read hd5 data
+df = pd.read_hdf(file)  # df: [gene, cell]
+print("reading complete")
+print("original data matrix shape: ", df.shape, "\n")
+
+if len(sys.argv) == 4:
+    n_rank = int(sys.argv[3])
+elif len(sys.argv) == 3:
+    n_rank = min(df.shape[0], df.shape[1]) - 1
+
+# Folder for SVD plots
+if not os.path.exists("svd_plots"):
+    os.makedirs("svd_plots")
+
+# Explained Variance ratio
+print("Calculating Variance ratio with TruncatedSVD")
+tic = time.clock()
+svd = TruncatedSVD(n_components=n_rank)
+svd.fit(df)
+toc = time.clock()
+print("TruncatedSVD took {:.1f} seconds\n".format(toc - tic))
+
+print("Plotting explained_variance_ratio_: ")
+# print(svd.explained_variance_ratio_[])
+fig = plt.figure(figsize=(9,9))
+plt.plot(svd.explained_variance_ratio_, 'b-')
+plt.xlabel('rank')
+plt.ylabel('explained_variance_ratio')
+plt.title("explained_variance_ratio")
+plt.savefig("./svd_plots/" + out_prefix + ".explained_variance_ratio.png", bbox_inches='tight')
+plt.close(fig)
+
+print("Plotting explained_variance_ratio_cumsum: ")
+explained_variance_ratio_cumsum = np.cumsum(svd.explained_variance_ratio_)
+# print(explained_variance_ratio_cumsum)
+fig = plt.figure(figsize=(9, 9))
+plt.plot(explained_variance_ratio_cumsum, 'r-')
+plt.xlabel('rank')
+plt.ylabel('explained_variance_ratio_cumsum')
+plt.title("explained_variance_ratio_cumsum")
+plt.savefig("./svd_plots/"+ out_prefix + ".explained_variance_ratio_cumsum.png", bbox_inches='tight')
+plt.close(fig)
