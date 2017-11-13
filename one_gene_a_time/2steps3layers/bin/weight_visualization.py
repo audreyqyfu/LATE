30cbc0b8dc526423088d86980c94c6f63e51d9b2
“Rui <liruiradiant@gmail.com>
2017-11-12 23:00:27 -0800

    works

---
+++ one_gene_a_time/2steps3layers/bin/weight_visualization.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# load weights from npy
+# matplotlib.pyplot.vis()
+# seaborn clustering and plotting
+
+
+import sys
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib
+# matplotlib.use('Agg')  # for plotting without GUI
+import matplotlib.pyplot as plt
+
+
+# read cmd
+print('usage: <weights_visualization.py> <w_name.npy> <out_tag>')
+if len(sys.argv) != 3:
+    raise Exception('cmd error')
+in_name = sys.argv[1]
+tag = sys.argv[2]
+
+
+# read data
+arr = np.load(in_name)
+print('matrix sample', arr[0:3, 0:3])
+print('matrix shape:', arr.shape)
+
+# seaborn clustering
+heatmap = sns.clustermap(arr)
+heatmap.savefig(in_name+'.'+tag+'.png', bbox_inches='tight')
+
