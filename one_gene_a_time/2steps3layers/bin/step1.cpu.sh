84252e3aab00beedf1204892929fb97e321508ba
“Rui <liruiradiant@gmail.com>
2017-11-14 09:24:46 -0800

    run.sh for stand along servers

---
+++ one_gene_a_time/2steps3layers/bin/step1.cpu.sh
@@ -0,0 +1,11 @@
+module load python/3.5.2
+echo "*--training NN--*"
+python -u ./step1.n_to_n.new.py
+
+echo "*--result analysis--*"
+python -u ./result_analysis.py step1
+
+echo "*--cluster map--*"
+for file in pre_train/*w*npy; do python weight_visualization.py $file sns_tag; done
+
+echo "*--done--*"
