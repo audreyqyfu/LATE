5a05ee645f285a3f0f90b67e495bb09d237f78b6
“Rui <liruiradiant@gmail.com>
2017-12-01 13:45:00 -0800

    asynchronized

--- one_gene_a_time/2steps3layers/bin/weight_visualization.step1.sh
+++ one_gene_a_time/2steps3layers/bin/weight_visualization.step1.sh
@@ -1,2 +1,5 @@
+echo 'doing clustering for all npy files, needs large RAM'
 module load python/3.5.2
-for file in step1/*npy; do python -u weight_visualization.py $file step1; done
+for file in step1/*npy
+do python -u weight_visualization.py $file step1 &
+done
--- one_gene_a_time/2steps3layers/bin/weight_visualization.step2.sh
+++ one_gene_a_time/2steps3layers/bin/weight_visualization.step2.sh
@@ -1,2 +1,5 @@
+echo 'doing clustering for all npy files, needs large RAM'
 module load python/3.5.2
-for file in step2/*npy; do python -u weight_visualization.py $file step2; done
+for file in step2/*npy
+do python -u weight_visualization.py $file step2 &
+done
