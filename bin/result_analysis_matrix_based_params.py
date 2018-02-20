8c893991e339ff858c1d2b54c1b7ee245649a834
“Rui <liruiradiant@gmail.com>
2018-02-19 20:58:34 -0800

    gtex_all: translate_5L

--- bin/result_analysis_matrix_based_params.py
+++ bin/result_analysis_matrix_based_params.py
@@ -1,13 +1,13 @@
 file_h = './step2/imputation.step2.hd5'
 file_h_ori = 'cell_row'  # gene_row/cell_row
 
-file_x = '../data/10x_human_pbmc_68k.g5561.hd5'
-file_x_ori = 'gene_row'
+file_x = '../data/gtex_v7.count.4tissues.msk90.hd5'
+file_x_ori = 'cell_row'
 
-file_m = '../data/10x_human_pbmc_68k.g5561.hd5'
-file_m_ori = 'gene_row'
+file_m = '../data/gtex_v7.count.4tissues_yes.hd5'
+file_m_ori = 'cell_row'
 
-tag = 'PBMC_G5561_CountLog_NoMsk_TransLate7L'
+tag = 'GTex_All_CountLog_Msk90_TransLate_5L'
 
 data_transformation = 'log'  # as_is/log/rpm_log/exp_rpm_log (done on H, X, M)
 
@@ -17,33 +17,33 @@ pair_list = [
     # [4058, 7496],
     # [8495, 12871],
     #
-    # # TEST
-    # [2, 3],
+    # TEST
+    [2, 3],
 
-    # PBMC G5561 Non-Linear
-    ['ENSG00000173372',
-    'ENSG00000087086'],
-
-    ['ENSG00000231389',
-    'ENSG00000090382'],
-
-    ['ENSG00000158869',
-    'ENSG00000090382'],
-
-    ['ENSG00000074800',
-    'ENSG00000019582'],
-
-    ['ENSG00000157873',
-    'ENSG00000169583'],
-
-    ['ENSG00000065978',
-    'ENSG00000139193'],
-
-    ['ENSG00000117450',
-    'ENSG00000133112'],
-
-    ['ENSG00000155366',
-    'ENSG00000167996'],
+    # # PBMC G5561 Non-Linear
+    # ['ENSG00000173372',
+    # 'ENSG00000087086'],
+    #
+    # ['ENSG00000231389',
+    # 'ENSG00000090382'],
+    #
+    # ['ENSG00000158869',
+    # 'ENSG00000090382'],
+    #
+    # ['ENSG00000074800',
+    # 'ENSG00000019582'],
+    #
+    # ['ENSG00000157873',
+    # 'ENSG00000169583'],
+    #
+    # ['ENSG00000065978',
+    # 'ENSG00000139193'],
+    #
+    # ['ENSG00000117450',
+    # 'ENSG00000133112'],
+    #
+    # ['ENSG00000155366',
+    # 'ENSG00000167996'],
 
 
 ]
@@ -52,33 +52,33 @@ gene_list = [
     # # MBM
     # 4058, 7496, 8495, 12871,
 
-    # # TEST
-    # 2, 3,
+    # TEST
+    2, 3,
     # 'ENSG00000188976', 'ENSG00000188290',
 
-    # PBMC G5561 Non-Linear
-    'ENSG00000173372',
-    'ENSG00000087086',
-
-    'ENSG00000231389',
-    'ENSG00000090382',
-
-    'ENSG00000158869',
-    'ENSG00000090382',
-
-    'ENSG00000074800',
-    'ENSG00000019582',
-
-    'ENSG00000157873',
-    'ENSG00000169583',
-
-    'ENSG00000065978',
-    'ENSG00000139193',
-
-    'ENSG00000117450',
-    'ENSG00000133112',
-
-    'ENSG00000155366',
-    'ENSG00000167996',
+    # # PBMC G5561 Non-Linear
+    # 'ENSG00000173372',
+    # 'ENSG00000087086',
+    #
+    # 'ENSG00000231389',
+    # 'ENSG00000090382',
+    #
+    # 'ENSG00000158869',
+    # 'ENSG00000090382',
+    #
+    # 'ENSG00000074800',
+    # 'ENSG00000019582',
+    #
+    # 'ENSG00000157873',
+    # 'ENSG00000169583',
+    #
+    # 'ENSG00000065978',
+    # 'ENSG00000139193',
+    #
+    # 'ENSG00000117450',
+    # 'ENSG00000133112',
+    #
+    # 'ENSG00000155366',
+    # 'ENSG00000167996',
 
 ]
--- bin/step1_params.py
+++ bin/step1_params.py
@@ -54,10 +54,10 @@ patience = 5  # early stop patience epochs, just print warning, early stop not i
 
 
 # GTEx
-file1 = '../data/gtex_v7.count.g5561.hd5'
+file1 = '../data/gtex_v7.count.hd5'
 data_transformation = 'log'  # as_is/log/rpm_log/exp_rpm_log
-name1 = 'GTEx.G5561.CountLog'
-file1_orientation = 'gene_row'  # cell_row/gene_row
+name1 = 'GTEx.All.CountLog'
+file1_orientation = 'cell_row'  # cell_row/gene_row
 
 # For development usage #
 seed_tf = 3
@@ -76,33 +76,33 @@ pair_list = [
     # [4058, 7496],
     # [8495, 12871],
 
-    # # TEST
-    # [2, 3],
+    # TEST
+    [2, 3],
 
-    # PBMC G5561 Non-Linear
-    ['ENSG00000173372',
-    'ENSG00000087086'],
-
-    ['ENSG00000231389',
-    'ENSG00000090382'],
-
-    ['ENSG00000158869',
-    'ENSG00000090382'],
-
-    ['ENSG00000074800',
-    'ENSG00000019582'],
-
-    ['ENSG00000157873',
-    'ENSG00000169583'],
-
-    ['ENSG00000065978',
-    'ENSG00000139193'],
-
-    ['ENSG00000117450',
-    'ENSG00000133112'],
-
-    ['ENSG00000155366',
-    'ENSG00000167996'],
+    # # PBMC G5561 Non-Linear
+    # ['ENSG00000173372',
+    # 'ENSG00000087086'],
+    #
+    # ['ENSG00000231389',
+    # 'ENSG00000090382'],
+    #
+    # ['ENSG00000158869',
+    # 'ENSG00000090382'],
+    #
+    # ['ENSG00000074800',
+    # 'ENSG00000019582'],
+    #
+    # ['ENSG00000157873',
+    # 'ENSG00000169583'],
+    #
+    # ['ENSG00000065978',
+    # 'ENSG00000139193'],
+    #
+    # ['ENSG00000117450',
+    # 'ENSG00000133112'],
+    #
+    # ['ENSG00000155366',
+    # 'ENSG00000167996'],
 
 
 ]
@@ -111,34 +111,34 @@ gene_list = [
     # # MBM
     # 4058, 7496, 8495, 12871,
 
-    # # TEST
-    # 2, 3,
+    # TEST
+    2, 3,
     # 'ENSG00000188976', 'ENSG00000188290',
 
-    # PBMC G5561 Non-Linear
-    'ENSG00000173372',
-    'ENSG00000087086',
-
-    'ENSG00000231389',
-    'ENSG00000090382',
-
-    'ENSG00000158869',
-    'ENSG00000090382',
-
-    'ENSG00000074800',
-    'ENSG00000019582',
-
-    'ENSG00000157873',
-    'ENSG00000169583',
-
-    'ENSG00000065978',
-    'ENSG00000139193',
-
-    'ENSG00000117450',
-    'ENSG00000133112',
-
-    'ENSG00000155366',
-    'ENSG00000167996',
+    # # PBMC G5561 Non-Linear
+    # 'ENSG00000173372',
+    # 'ENSG00000087086',
+    #
+    # 'ENSG00000231389',
+    # 'ENSG00000090382',
+    #
+    # 'ENSG00000158869',
+    # 'ENSG00000090382',
+    #
+    # 'ENSG00000074800',
+    # 'ENSG00000019582',
+    #
+    # 'ENSG00000157873',
+    # 'ENSG00000169583',
+    #
+    # 'ENSG00000065978',
+    # 'ENSG00000139193',
+    #
+    # 'ENSG00000117450',
+    # 'ENSG00000133112',
+    #
+    # 'ENSG00000155366',
+    # 'ENSG00000167996',
 
 ]
 
