0affdcdcd60a82ee80baf12bbe8c126081d3e145
“Rui <liruiradiant@gmail.com>
2018-05-11 11:01:54 -0700

    misc

--- bin/misc/sacct.sh
+++ bin/misc/sacct.sh
@@ -2,3 +2,8 @@
 # see Mem usage of job submitted to slurm GPUs
 sacct --format="JobID, JobName, Partition, AllocCPUS, ReqMem, AveVmSize,
 MaxVmSize, State, ExitCode"
+
+# LOG RAM USAGE, AND MORE
+sacct --format="JobID, JobName, Partition, AllocCPUS, ReqMem, AveVmSize, \
+MaxVmSize, State, ExitCode" > sacct.txt
+sacct --format="ALL" >> sacct.txt
