c6c6c915ab6ab77afe89b674987f95e274c241a8
“Rui <liruiradiant@gmail.com>
2018-05-11 13:47:49 -0700

    color tsne by cluster for pbmc

--- params/params_scVI_pbmc_g949_c21k.py
+++ params/params_scVI_pbmc_g949_c21k.py
@@ -23,7 +23,7 @@ transformation_ground_truth = transformation_input  # as_is/log/rpm_log/exp_rpm_
 # For result analysis
 cluster_file = home + '/data/pbmc_40kC_10clusters.csv'
 
-fname_imputation = 'pbmc.g949_c10k.msk90.px_rate.csv.gz'  # can be changed
+fname_imputation = 'pbmc.g949_c21k.msk90.px_rate.csv.gz'  # can be changed
 name_imputation = '{}_({})'.format(name_input, mode)
 ori_imputation = 'cell_row'  # gene_row/cell_row
 transformation_imputation = 'log'
