88e5655d145dea46c3b5facae2e640ce5458b76b
“Rui <liruiradiant@gmail.com>
2018-05-29 11:57:07 -0700

    version fix (v1.0 rather than newest) and ensure gene_row input

--- other_imputations/saver/saver1.0.R
+++ other_imputations/saver/saver1.0.R
@@ -2,15 +2,17 @@
 # Objective : Compare with Saver1.0
 # Created by: rui
 # Created on: 5/9/18
-
-# devtools::install_github("mohuangx/SAVER")
+devtools::install_github("mohuangx/SAVER@v1.0.0")
 library('SAVER')
 getwd()
+packageVersion('SAVER')
+sessionInfo()  # see version
 
 # read data (COUNT)
 in_file = '~/data/cell_row/pbmc.g949_c10k.msk90.csv.gz'
 out_file = 'pbmc_g949_c10k.msk.saver.csv.gz'
 df = read.csv(in_file, row.names=1)
+df = t(df)  # into gene_row
 df[0:3, 0:3]
 
 # single core
