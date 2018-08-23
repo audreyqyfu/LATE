bafb4238d1dccd3f9f8993a8762b542ea3f4c921
rui <liruiradiant@gmail.com>
2018-08-23 08:04:04 -0400

    added os.path.expanduser, to make sure '~' read as home dir

--- scripts/params.py
+++ scripts/params.py
@@ -9,21 +9,21 @@ mode = 'late'  # takes pre-training, translate, late, impute for late.py; takes 
 # result_analysis.py
 
 # Input (should contain index and header)
-fname_input = '../data/cell_row/example.msk90.hd5'  # csv/csv.gz/tsv/h5/hd5 formats supported
-name_input = 'example'
+fname_input = os.path.expanduser('~/data/cell_row/example.msk90.hd5')
+name_input = 'example.msk90'
 ori_input = 'cell_row'  # cell_row/gene_row
 transformation_input = 'log10'  # as_is/log10/rpm_log10/exp_rpm_log10
 
 # Output
-fname_imputation = './step2/imputation.step2.hd5'  # do not modify for pre-training
+fname_imputation = os.path.expanduser('./step2/imputation.step2.hd5')  # do not modify for pre-training
 name_imputation = '{}_({})'.format(name_input, mode)  # recommend not to modify
 ori_imputation = 'cell_row'  # gene_row/cell_row
 transformation_imputation = 'as_is'  # log10/rpm_log10/exp_rpm_log10
 tag = 'Eval'  # folder name for analysis results
 
 # Ground Truth
-fname_ground_truth = fname_input
-name_ground_truth = name_input
+fname_ground_truth = os.path.expanduser('~/data/cell_row/example.hd5')
+name_ground_truth = 'GT'
 ori_ground_truth = ori_input
 transformation_ground_truth = transformation_input
 
@@ -62,9 +62,9 @@ else:
 
 # HYPER PARAMETERS
 # Model structure
-L = 5  # num of layers for Autoencoder, accept: 3/5/7
+L = 3  # num of layers for Autoencoder, accept: 3/5/7
 l = L//2  # inner usage, do not modify
-n_hidden_1 = 800  # needed for 3/5/7 layer design
+n_hidden_1 = 200  # needed for 3/5/7 layer design
 n_hidden_2 = 400  # needed for 5/7 layer design
 # n_hidden_3 = 200 # needed for 7 layer design
 
@@ -83,7 +83,7 @@ sample_size = int(1000)  # sample_size for learning curve, slow output
 large_size = int(1e5)  # if num-cells larger than this, use slow but robust method for imputation and output
 
 # Length of training
-max_training_epochs = int(100)  # num_mini_batch / (training_size/batch_size)
+max_training_epochs = int(100)  # num_mini_batch / (training_size/batch_size)  todo: change to 100
 display_step = int(5)  # interval on learning curve, 20 displays recommended
 snapshot_step = int(50)  # interval of saving session, saving imputation
 patience = int(3)  # early stop patience epochs, just print warning, no real stop
@@ -95,7 +95,7 @@ reg_coef = 0.0  # reg3=1e-2, set to 0.0 to desable it
 
 # For development usage
 seed_tf = 3
-test_flag = False  # [True, False]
+test_flag = True  # [True, False]
 if test_flag:
     max_training_epochs = 10 # 3L:100, 5L:1000, 7L:1000, 9L:3000
     display_step = 1  # interval on learning curve
